748d1666ada33f53590f2ae0243465fba9592b71
bandwich <>
2023-06-13 14:46:48 -0400

    updated encodings, see wiki

--- AutoController.js
+++ AutoController.js
@@ -10,48 +10,48 @@ AutoController.init = init;
 AutoController.shutdown = doNothing;
 
 AutoController.timers = {
-    gain: -1,
-    volumeA: -1,
-    volumeB: -1,
-    parameter1A: -1,
-    parameter1B: -1,
-    parameter2A: -1,
-    parameter2B: -1,
-    parameter3A: -1,
-    parameter3B: -1,
-    bpmA: -1,
-    bpmB: -1
+    gain: 0,
+    volumeA: 0,
+    volumeB: 0,
+    parameter1A: 0,
+    parameter1B: 0,
+    parameter2A: 0,
+    parameter2B: 0,
+    parameter3A: 0,
+    parameter3B: 0,
+    bpmA: 0,
+    bpmB: 0
+}
+
+const _stopTimer = function(timerId) {
+    engine.stopTimer(timerId);
 }
 
 var scrollPosition = 1;
 const valueAnswerStatus = 0x80;
 const timeAnswerStatus = 0x81;
-const _master = "[Master]";
-const _library = "[Library]";
+const _masterString = "[Master]";
+const _libraryString = "[Library]";
 const sortByPosition = 23;
-const _channel = function(number) {
+const _channelString = function(number) {
     return "[Channel" + (number + 1).toString() + "]";
 }
 
 function init() {
-    engine.setValue(_library, 'sort_column', sortByPosition); // sort by position
-    engine.setValue(_library, 'sort_order', 0); // sort ascending
+    engine.setValue(_libraryString, 'sort_column', sortByPosition); // sort by position
+    engine.setValue(_libraryString, 'sort_order', 0); // sort ascending
 }
 
-const _eq = function(number) {
-    return "[EqualizerRack1_" + _channel(number) + "_Effect1]";
+const _eqString = function(number) {
+    return "[EqualizerRack1_" + _channelString(number) + "_Effect1]";
 }
 
 const _hotcue = function(number) {
     return "hotcue_" + number + "_activate";
 }
 
-const _nudge = function(direction, size, group) {
-    if (direction < 1) {
-        return size ? group.concat('_down') : group.concat('_down_small');
-    } else {
-        return size ? group.concat('_up') : group.concat('_up_small');
-    }
+const _nudge = function(direction, group) {
+    return direction ? group.concat('_up') : group.concat('_down');
 }
 
 // deck-dependent groups have a deck suffix
@@ -67,23 +67,23 @@ const _affixGroup = function(group, suffix) {
 
 const _checkTimer = function(engine, group) {
     // this if check may be unnecessary
-    if (AutoController.timers[group] !== -1) {
-        engine.stopTimer(AutoController.timers[group]);
-        AutoController.timers[group] = -1;
+    if (AutoController.timers[group] !== 0) {
+       _stopTimer(AutoController.timers[group]);
+       AutoController.timers[group] = 0;
     }
 }
 
 const _navigateToPlaylist = function(id) {
     // from Mixxx launch
-    engine.setValue(_library, "MoveDown", 1);
-    engine.setValue(_library, 'MoveDown', 1);
-    engine.setValue(_library, 'GoToItem', 1);
+    engine.setValue(_libraryString, "MoveDown", 1);
+    engine.setValue(_libraryString, 'MoveDown', 1);
+    engine.setValue(_libraryString, 'GoToItem', 1);
     _selectPlaylist(id);
 }
 
 const _selectPlaylist = function(id) {
     for (var i=0; i<id; i++) {
-        engine.setValue(_library, 'MoveDown', 1);
+        engine.setValue(_libraryString, 'MoveDown', 1);
     }
     // seems 1000ms is enough of a threshold for actions to update first
     engine.beginTimer(1000, init, true);
@@ -91,9 +91,8 @@ const _selectPlaylist = function(id) {
 }
 
 const _focusNode = function() {
-    engine.setValue(_library, 'MoveFocusForward', 1);
+    engine.setValue(_libraryString, 'MoveFocusForward', 1);
 }
-
 
 /* ------------------------------------------------------------------------------ */
 
@@ -107,60 +106,51 @@ AutoController.selectTrack = function(channel, deck, value, status, group) {
     const diff = value - scrollPosition;
     scrollPosition = value;
     
-    engine.setValue(_library, 'MoveVertical', diff);
-    engine.setValue(_channel(deck), group, 1);
+    engine.setValue(_libraryString, 'MoveVertical', diff);
+    engine.setValue(_channelString(deck), group, 1);
 }
 
 AutoController.cancelNudge = function(channel, control, value, status, group) {
-    if (AutoController.timers[group] === -1) {
+    if (AutoController.timers[group] === 0) {
         print("Careful: timer for " + group + " is not yet in use.");
         return;
     }
     _checkTimer(engine, group);
 }
 
-AutoController.nudgeMaster = function(channel, size, direction, status, group) {
-    _checkTimer(engine, group);
-    const control =  _nudge(direction, size, group);
-    const timerId = engine.beginTimer(200, function () {
-
-        // Don't see it documented by Mixxx, but triggerControl runs a one-shot timer under the hood
-        // triggerControl takes in a delay as its last param, in ms
-        // 20ms is the fastest resolution
-        script.triggerControl(_master, control, 20);
-    }, false);
-    AutoController.timers[group] = timerId;
+// maps a value into an inverted new range
+const remapFlip = function map_range(value, low1, high1, high2, low2) {
+    return high2 - (value / high1) * (high2 - low2);
 }
 
 AutoController.nudge = function(channel, deck, val, status, group) {
-    _checkTimer(engine, group);
+    const direction = val > 64;
+    const min = (val < 64) ? 0 : 64;
+    const max = (val < 64) ? 64 : 127;
+    const speed = remapFlip(val, min, max, 500, 75);
 
-    // deck-dependent nudges encode both direction and size within val
-    // even: nudge down, odd: nudge up ... 0-1: 1% adjust, 2-3: 4% adjust
-    const control = _nudge(val % 2, Math.floor(val / 2), _stripGroup(group));
-    const timerId = engine.beginTimer(200, function () {
-        script.triggerControl(_channel(deck), control, 20);
-    }, false);
-    AutoController.timers[group] = timerId;
-}
-
-// A generalized nudge function with pattern matching would help
-AutoController.nudgeEQ = function(channel, deck, val, status, group) {
     _checkTimer(engine, group);
-   
-    const control = _nudge(val % 2, Math.floor(val / 2), _stripGroup(group));
-    const timerId = engine.beginTimer(200, function () {
-        script.triggerControl(_eq(deck), control, 20);
+    print("Status: " + status);
+
+    const control = _nudge(direction, _stripGroup(group));
+    var groupString = _channelString(deck);
+    if (status >= 0x97 && status <= 0x99) {
+        groupString = _eqString(deck);
+    } else if (status === 0x9b) {
+        groupString = _masterString;
+    }
+    const timerId = engine.beginTimer(speed, function () {
+         script.triggerControl(groupString, control, 20);
     }, false);
     AutoController.timers[group] = timerId;
 }
 
 AutoController.set = function(channel, deck, value, status, group) {
-    engine.setValue(_channel(deck), group, value);
+    engine.setValue(_channelString(deck), group, value);
 }
 
 AutoController.activateHotcue = function(channel, deck, number) {
-    engine.setValue(_channel(deck), _hotcue(number), 1);
+    engine.setValue(_channelString(deck), _hotcue(number), 1);
 }
 
 // reports requested single value back on the same channel
@@ -172,7 +162,7 @@ AutoController.askSingle = function(channel, deck, value, status, group) {
         const second = val - first;
         return [first, second];
     }
-    const vals = split(engine.getValue(_channel(deck), group));
+    const vals = split(engine.getValue(_channelString(deck), group));
     midi.sendShortMsg(valueAnswerStatus, vals[0], vals[1]);
 }
 
@@ -185,6 +175,6 @@ AutoController.askTime = function(channel, deck, value, status, group) {
         const seconds = val % 60;
         return [minutes, seconds];
     }
-    const vals = split(engine.getValue(_channel(deck), group));
+    const vals = split(engine.getValue(_channelString(deck), group));
     midi.sendShortMsg(timeAnswerStatus, vals[0], vals[1]);
 }
